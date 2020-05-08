b515fb7bbd3a216e6dc7b8afa7ab8efe9a4ea1ab
songmeo <shuong1012@gmail.com>
2020-05-08 16:17:02 +0300

    change pipe

--- ICS0025.cpp
+++ ICS0025.cpp
@@ -31,115 +31,57 @@ void main(int argc, char** argv) {
 	Item* itm = new Item('C', 2, "A", *(new Date(1, 10, 2000)));
 	d->InsertSubgroup('C', 2, { itm });
 */
-int _tmain(int argc, TCHAR * argv[])
+
+int main()
 {
-    HANDLE hPipe;
-    LPCTSTR lpvMessage = TEXT("ready");
-    TCHAR  chBuf[BUFSIZE];
-    BOOL   fSuccess = FALSE;
-    DWORD  cbRead, cbToWrite, cbWritten, dwMode;
-    LPCTSTR lpszPipename = TEXT("\\\\.\\pipe\\ICS0025");
+	HANDLE hPipe;
+	//LPCTSTR lpvMessage = TEXT("ready");
+	//TCHAR  chBuf[BUFSIZE];
+	//BOOL   fSuccess = FALSE;
+	//DWORD  cbRead, cbToWrite, cbWritten, dwMode;
+	LPCTSTR lpszPipename = TEXT("\\\\.\\pipe\\ICS0025");
 
-    if (argc > 1)
-        lpvMessage = argv[1];
+	hPipe = CreateFile(
+		lpszPipename,   // pipe name 
+		GENERIC_READ |  // read and write access 
+		GENERIC_WRITE,
+		0,              // no sharing 
+		NULL,           // default security attributes
+		OPEN_EXISTING,  // opens existing pipe 
+		0,              // default attributes 
+		NULL);          // no template file 
 
-    // Try to open a named pipe; wait for it, if necessary. 
-
-    while (1)
-    {
-        hPipe = CreateFile(
-            lpszPipename,   // pipe name 
-            GENERIC_READ |  // read and write access 
-            GENERIC_WRITE,
-            0,              // no sharing 
-            NULL,           // default security attributes
-            OPEN_EXISTING,  // opens existing pipe 
-            0,              // default attributes 
-            NULL);          // no template file 
-
-        // Break if the pipe handle is valid. 
-
-        if (hPipe != INVALID_HANDLE_VALUE)
-            break;
-
-        // Exit if an error other than ERROR_PIPE_BUSY occurs. 
-
-        if (GetLastError() != ERROR_PIPE_BUSY)
-        {
-            _tprintf(TEXT("Could not open pipe. GLE=%d\n"), GetLastError());
-            return -1;
-        }
-
-        // All pipe instances are busy, so wait for 20 seconds. 
-
-        if (!WaitNamedPipe(lpszPipename, 20000))
-        {
-            printf("Could not open pipe: 20 second wait timed out.");
-            return -1;
-        }
-    }
-
-    // The pipe connected; change to message-read mode. 
-
-    dwMode = PIPE_READMODE_MESSAGE;
-    fSuccess = SetNamedPipeHandleState(
-        hPipe,    // pipe handle 
-        &dwMode,  // new pipe mode 
-        NULL,     // don't set maximum bytes 
-        NULL);    // don't set maximum time 
-    if (!fSuccess)
-    {
-        _tprintf(TEXT("SetNamedPipeHandleState failed. GLE=%d\n"), GetLastError());
-        return -1;
-    }
-
-    // Send a message to the pipe server. 
-
-    cbToWrite = (lstrlen(lpvMessage) + 1) * sizeof(TCHAR);
-    _tprintf(TEXT("Sending %d byte message: \"%s\"\n"), cbToWrite, lpvMessage);
-
-    fSuccess = WriteFile(
-        hPipe,                  // pipe handle 
-        lpvMessage,             // message 
-        cbToWrite,              // message length 
-        &cbWritten,             // bytes written 
-        NULL);                  // not overlapped 
-
-    if (!fSuccess)
-    {
-        _tprintf(TEXT("WriteFile to pipe failed. GLE=%d\n"), GetLastError());
-        return -1;
-    }
-
-    printf("\nMessage sent to server, receiving reply as follows:\n");
-
-    do
-    {
-        // Read from the pipe. 
-
-        fSuccess = ReadFile(
-            hPipe,    // pipe handle 
-            chBuf,    // buffer to receive reply 
-            BUFSIZE * sizeof(TCHAR),  // size of buffer 
-            &cbRead,  // number of bytes read 
-            NULL);    // not overlapped 
-
-        if (!fSuccess && GetLastError() != ERROR_MORE_DATA)
-            break;
-
-        _tprintf(TEXT("\"%s\"\n"), chBuf);
-    } while (!fSuccess);  // repeat loop if ERROR_MORE_DATA 
-
-    if (!fSuccess)
-    {
-        _tprintf(TEXT("ReadFile from pipe failed. GLE=%d\n"), GetLastError());
-        return -1;
-    }
-
-    printf("\n<End of message, press ENTER to terminate connection and exit>");
-    _getch();
-
-    CloseHandle(hPipe);
-
-    return 0;
+	if (hPipe == INVALID_HANDLE_VALUE)
+	{
+		cout << "Unable to create file, error " << GetLastError() << endl;
+		return 1;
+	}
+	int* pData1 = new int[10], i;
+	unsigned long nWritten;
+	for (i = 0; i < 10; i++)
+		*(pData1 + i) = i;
+	if (!WriteFile(hPipe, pData1, 10 * sizeof(int), &nWritten, NULL))
+	{
+		cout << "Unable to write into file, error " << GetLastError() << endl;
+		return 1;
+	}
+	if (nWritten != 10 * sizeof(int))
+		cout << "Only " << nWritten << " bytes were written" << endl;
+	int* pData2 = new int[10];
+	unsigned long nRead;
+	if (SetFilePointer(hPipe, 0, NULL, FILE_BEGIN) == INVALID_SET_FILE_POINTER)
+	{
+		cout << "Unable to set the file pointer, error " << GetLastError() << endl;
+		return 1;
+	}
+	if (!ReadFile(hPipe, pData2, nWritten, &nRead, NULL))
+	{
+		cout << "Unable to read from file, error " << GetLastError() << endl;
+		return 1;
+	}
+	for (i = 0; i < 10; i++)
+		cout << pData2[i] << ' ';
+	cout << endl;
+	CloseHandle(hPipe);
+	return 0;
 }
