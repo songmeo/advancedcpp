fe0e9f3aacd1ef0dcb5a67b1ef9ab072cc433f34
songmeo <shuong1012@gmail.com>
2020-05-16 23:34:41 +0300

    server doesn't take msg

--- ICS0025.cpp
+++ ICS0025.cpp
@@ -16,53 +16,95 @@
 
 using namespace std;
 
-enum inputs{ready, connect, stop};
-const char* c = "connect";
-const char* r = "ready";
-const char* s = "stop";
+enum inputs{r, c, s};
+const char* con = "connect";
+const char* ready = "ready";
+const char* stop = "stop";
 unsigned long nWritten = 0;
+unsigned long nRead = 0;
 char* reply = new char[BUFSIZE];
 
 void takeInput(queue<inputs> &q) {
-	string input;
-	while (input != "stop") {
+	inputs i = c;
+	string tmp;
+	while (i != s) {
 		cout << "Enter input: ";
-		cin >> input;
+		cin >> tmp;
+		if (tmp == "ready") {
+			i = r;
+		}
+		else if (tmp == "connect") {
+			i = c;
+		}
+		else if (tmp == "stop") {
+			i = s;
+		}
+		q.push(i);
 	}
 }
 
-void sendMsg(HANDLE hPipe, queue<inputs>& q) {
+void sendMsg(HANDLE hPipe, queue<inputs> &q) {
+	const char* input = nullptr;
 	while (1) {
 		while (q.empty()) {
-			this_thread::sleep_for(chrono::milliseconds(100)); //1/10 second
+			this_thread::sleep_for(chrono::milliseconds(100)); //wait for input
 		}
 		inputs i = q.front();
 		switch (i) {
-		case connect:
-			if (!WriteFile(hPipe, c, strlen(c) + 1, &nWritten, NULL))
-			{
-				cout << "Unable to write into file, error " << GetLastError() << endl;
-			}
-			if (nWritten != strlen(c) + 1)
-				cout << "Only " << nWritten << " bytes were written" << endl;
-		case ready:
-			if (!WriteFile(hPipe, r, strlen(c) + 1, &nWritten, NULL))
-			{
-				cout << "Unable to write into file, error " << GetLastError() << endl;
-			}
-			if (nWritten != strlen(r) + 1)
-				cout << "Only " << nWritten << " bytes were written" << endl;
-		case stop:
-			if (!WriteFile(hPipe, s, strlen(c) + 1, &nWritten, NULL))
-			{
-				cout << "Unable to write into file, error " << GetLastError() << endl;
-			}
-			if (nWritten != strlen(s) + 1)
-				cout << "Only " << nWritten << " bytes were written" << endl;
+		case c:
+			input = con;
+			
+			break;
+		case r:
+			input = ready;
+			break;
+		case s:
+			input = stop;
+		}
+		if (!WriteFile(hPipe, input, strlen(input) + 1, &nWritten, NULL))
+		{
+			cout << "Unable to write into file, error " << GetLastError() << endl;
+			return;
+		}
+		if (nWritten != strlen(input) + 1) {
+			cout << "Only " << nWritten << " bytes were written" << endl;
+			return;
 		}
 		q.pop();
 	}
-	
+}
+
+void takeReply(HANDLE hPipe) {
+	if (!ReadFile(hPipe, reply, BUFSIZE, &nRead, NULL))
+	{
+		cout << "Unable to read from file, error " << GetLastError() << endl;
+		return;
+	}
+	string str = reply;
+
+	//parse group, subgroup
+	char group = str[0];
+	int subgroup = str[2];
+
+	//parse name
+	int open = str.find('<');
+	int close = str.find('>');
+	string name = str.substr(open + 1, close - open - 1);
+
+	//parse date
+	stringstream ss(str.substr(close + 2));
+	int d;
+	string m, y;
+	ss >> d >> m >> y;
+	vector<string> months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+	int month = find(months.begin(), months.end(), m) - months.begin() + 1;
+	int year = stoi(y);
+
+	//make new date
+	Date* date = new Date(d, month, year);
+
+	//make new item
+	Item* itm = new Item(group, subgroup, name, *date);
 }
 
 int main()
@@ -83,76 +125,19 @@ int main()
 		cout << "Unable to create file, error " << GetLastError() << endl;
 		return 1;
 	}
-	
-	if (!WriteFile(hPipe, r, strlen(r) + 1, &nWritten, NULL))
-	{
-		cout << "Unable to write into file, error " << GetLastError() << endl;
-		return 1;
-	}
-	if (nWritten != strlen(r) + 1)
-		cout << "Only " << nWritten << " bytes were written" << endl;
-	
-	unsigned long nRead;
 	if (SetFilePointer(hPipe, 0, NULL, FILE_BEGIN) == INVALID_SET_FILE_POINTER)
 	{
 		cout << "Unable to set the file pointer, error " << GetLastError() << endl;
 		return 1;
 	}
-	if (!ReadFile(hPipe, reply, BUFSIZE, &nRead, NULL))
-	{
-		cout << "Unable to read from file, error " << GetLastError() << endl;
-		return 1;
-	}
-	string str = reply;
-
-	//parse group, subgroup
-	char group = str[0];
-	int subgroup = str[2];
-	
-	//parse name
-	int open = str.find('<');
-	int close = str.find('>');
-	string name = str.substr(open + 1, close - open - 1);
-	
-	//parse date
-	stringstream ss(str.substr(close + 2));
-	int d;
-	string m, y;
-	ss >> d >> m >> y;
-	vector<string> months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
-	int month = find(months.begin(), months.end(), m) - months.begin() + 1;
-	int year = stoi(y);
-	
-	//make new date
-	Date* date = new Date(d, month, year);
-
-	//make new item
-	Item* itm = new Item(group, subgroup, name, *date);
-	
-	//send ready after making new item
-	if (!WriteFile(hPipe, ready, strlen(ready) + 1, &nWritten, NULL))
-	{
-		cout << "Unable to write into file, error " << GetLastError() << endl;
-		return 1;
-	}
-	if (nWritten != strlen(ready) + 1)
-		cout << "Only " << nWritten << " bytes were written" << endl;
-	
-	//send stop
-	if (!WriteFile(hPipe, stop, strlen(stop) + 1, &nWritten, NULL))
-	{
-		cout << "Unable to write into file, error " << GetLastError() << endl;
-		return 1;
-	}
-	if (nWritten != strlen(stop) + 1)
-		cout << "Only " << nWritten << " bytes were written" << endl;
-	
 	queue<inputs> q;
-	thread listenInput{ takeInput, q };
-	thread sendServer{ sendMsg, q };
-}
-
+	thread listenInput{ takeInput, ref(q) };
+	thread sendServer{ sendMsg, hPipe, ref(q) };
+	thread takeOutput(takeReply, hPipe);
 	listenInput.join();
-	CloseHandle(hPipe);
+	sendServer.join();
+	takeOutput.join();
+
+	//CloseHandle(hPipe);
 	return 0;
 }
