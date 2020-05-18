fa9d7829ff8987ed4ac26e16ae6d8f4ecd64a2ae
songmeo <shuong1012@gmail.com>
2020-05-18 15:11:59 +0300

    async read and write

--- ICS0025.cpp
+++ ICS0025.cpp
@@ -70,22 +70,18 @@ void sendMsg(HANDLE hPipe, queue<inputs> &q) {
 	}
 }
 
-void takeReply(HANDLE hPipe, HANDLE hExitEvent) {
-	OVERLAPPED Overlapped;
-	memset(&Overlapped, 0, sizeof Overlapped);
+void takeReply(HANDLE hPipe) {
 	bool NoData = true;
-	Overlapped.hEvent = CreateEventA(NULL, FALSE, FALSE, NULL);
-	HANDLE hEvents[] = { Overlapped.hEvent, hExitEvent };
-	if (!ReadFile(hPipe, reply, BUFSIZE, &nRead, &Overlapped))
+	if(!ReadFile(hPipe, reply, BUFSIZE, &nRead, NULL))
 	{
 		int error = GetLastError();
 		switch (error)
 		{
 		case ERROR_IO_PENDING:
-			switch (WaitForMultipleObjects(2, hEvents, FALSE, TIMEOUT))
+			switch (WaitForMultipleObjects(2, NULL, FALSE, TIMEOUT))
 			{ // waiting for response from COM1
 			case WAIT_OBJECT_0:
-				GetOverlappedResult(hPipe, &Overlapped, &nRead, FALSE);
+				GetOverlappedResult(hPipe, NULL, &nRead, FALSE);
 				NoData = false; // Got some data, waiting ended
 				break;
 			case WAIT_OBJECT_0 + 1:
@@ -103,39 +99,33 @@ void takeReply(HANDLE hPipe, HANDLE hExitEvent) {
 			cout << "Reading failed, error " << GetLastError() << ". Press a key to exit" << endl;
 			break;
 		}
-		return;
 	}
-	else
-	{ // has read immediately
-		NoData = false;
-	}
-	if (!NoData) {
-		string str = reply;
+	cout << "Hi";
+	string str = reply;
 
-		//parse group, subgroup
-		char group = str[0];
-		int subgroup = str[2];
+	//parse group, subgroup
+	char group = str[0];
+	int subgroup = str[2];
 
-		//parse name
-		int open = str.find('<');
-		int close = str.find('>');
-		string name = str.substr(open + 1, close - open - 1);
+	//parse name
+	int open = str.find('<');
+	int close = str.find('>');
+	string name = str.substr(open + 1, close - open - 1);
 
-		//parse date
-		stringstream ss(str.substr(close + 2));
-		int d;
-		string m, y;
-		ss >> d >> m >> y;
-		vector<string> months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
-		int month = find(months.begin(), months.end(), m) - months.begin() + 1;
-		int year = stoi(y);
+	//parse date
+	stringstream ss(str.substr(close + 2));
+	int d;
+	string m, y;
+	ss >> d >> m >> y;
+	vector<string> months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+	int month = find(months.begin(), months.end(), m) - months.begin() + 1;
+	int year = stoi(y);
 
-		//make new date
-		Date* date = new Date(d, month, year);
+	//make new date
+	Date* date = new Date(d, month, year);
 
-		//make new item
-		Item* itm = new Item(group, subgroup, name, *date);
-	}
+	//make new item
+	Item* itm = new Item(group, subgroup, name, *date);
 }
 
 int main()
@@ -148,7 +138,7 @@ int main()
 		0,              // no sharing 
 		NULL,           // default security attributes
 		OPEN_EXISTING,  // opens existing pipe 
-		0, // for asynchronous writing and reading FILE_FLAG_OVERLAPPED
+		FILE_FLAG_OVERLAPPED, // for asynchronous writing and reading 
 		NULL);          // no template file 
 
 	if (hPipe == INVALID_HANDLE_VALUE)
@@ -156,12 +146,13 @@ int main()
 		cout << "Unable to create file, error " << GetLastError() << endl;
 		return 1;
 	}
-	if (SetFilePointer(hPipe, 0, NULL, FILE_BEGIN) == INVALID_SET_FILE_POINTER)
-	{
-		cout << "Unable to set the file pointer, error " << GetLastError() << endl;
-		return 1;
-	}
+	//if (SetFilePointer(hPipe, 0, NULL, FILE_BEGIN) == INVALID_SET_FILE_POINTER)
+	//{
+	//	cout << "Unable to set the file pointer, error " << GetLastError() << endl;
+	//	return 1;
+	//}
 	queue<inputs> q;
+	//HANDLE hExitEvent = CreateEventA(NULL, TRUE, FALSE, NULL);
 	thread listenInput{ takeInput, ref(q) };
 	thread sendServer{ sendMsg, hPipe, ref(q) };
 	thread takeOutput(takeReply, hPipe);
