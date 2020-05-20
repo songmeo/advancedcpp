c7a6ec90f78698f44f717c1e98da351f60ffe55e
songmeo <shuong1012@gmail.com>
2020-05-21 02:03:35 +0300

    remove write overlap

--- ICS0025.cpp
+++ ICS0025.cpp
@@ -48,116 +48,89 @@ void takeInput(queue<inputs> &q, HANDLE hHaveInput, HANDLE hExitEvent) {
 	}
 }
 
-void sendMsg(HANDLE hPipe, queue<inputs> &q, HANDLE hExitEvent) {
-	const char* input = nullptr;
-	while (1) {
-		if (q.empty()) {
-			this_thread::sleep_for(chrono::milliseconds(100)); //wait for input
-			continue;
-		}
-		inputs i = q.front();
-		switch (i) {
-		case c:
-			input = ready;
-			break;
-		case s:
-			input = stop;
-			break;
-		}
-		if (!WriteFile(hPipe, input, strlen(input) + 1, &nWritten, NULL))
-		{
-			cout << "Unable to write into file, error " << GetLastError() << endl;
-			return;
-		}
-		if (nWritten != strlen(input) + 1) {
-			cout << "Only " << nWritten << " bytes were written" << endl;
-			return;
-		}
-		q.pop();
-	}
-}
+//void sendMsg(HANDLE hPipe, queue<inputs> &q, HANDLE hExitEvent) {
+//	const char* input = nullptr;
+//	while (1) {
+//		if (q.empty()) {
+//			this_thread::sleep_for(chrono::milliseconds(100)); //wait for input
+//			continue;
+//		}
+//		inputs i = q.front();
+//		switch (i) {
+//		case c:
+//			input = ready;
+//			break;
+//		case s:
+//			input = stop;
+//			break;
+//		}
+//		if (!WriteFile(hPipe, input, strlen(input) + 1, &nWritten, NULL))
+//		{
+//			cout << "Unable to write into file, error " << GetLastError() << endl;
+//			return;
+//		}
+//		if (nWritten != strlen(input) + 1) {
+//			cout << "Only " << nWritten << " bytes were written" << endl;
+//			return;
+//		}
+//		q.pop();
+//	}
+//}
 
 
 void sendMsg(HANDLE hPipe, queue<inputs>& q, HANDLE hHaveInput, HANDLE hExitEvent) {
 	OVERLAPPED Overlapped;
 	memset(&Overlapped, 0, sizeof Overlapped);
-	Overlapped.hEvent = CreateEventA(NULL, FALSE, FALSE, NULL);
-	HANDLE hEvents[] = { Overlapped.hEvent, hHaveInput, hExitEvent }; //either it gets data or timeout
+	//Overlapped.hEvent = CreateEventA(NULL, FALSE, FALSE, NULL);
+	HANDLE hEvents[] = { hHaveInput, hExitEvent }; //either it gets data or timeout
 	bool NoData = true;
 	bool exit = false;
+	const char* input = nullptr;
 	while (1) {
-		if (!WriteFile(hPipe, input, strlen(input) + 1, &nWritten, Overlapped))
-		{
-			int error = GetLastError();
-			switch (error)
-			{
-			case ERROR_IO_PENDING:
-				switch (WaitForMultipleObjects(2, hEvents, FALSE, TIMEOUT))
-				{
-				case WAIT_OBJECT_0:
-					GetOverlappedResult(hPipe, &Overlapped, &nRead, FALSE);
-					NoData = false; // Got some data, waiting ended
-					break;
-				case WAIT_OBJECT_0 + 1:
-					cout << "Reading broken off" << endl;
-					exit = true;
-					break; // user has broken the ending off
-				case WAIT_TIMEOUT:
-					cout << "Timeout period " << TIMEOUT << "ms elapsed, nothing was received. Press a key to exit" << endl;
-					exit = true;
-					break; // timeout
-				default:
-					cout << "Reading failed, error " << GetLastError() << ". Press a key to exit" << endl;
-					exit = true;
-					break; // some system errors
-				}
+		switch(WaitForMultipleObjects(2, hEvents, FALSE, TIMEOUT)) {
+			case WAIT_OBJECT_0:
+				NoData = false; // Got some data, waiting ended
 				break;
-			default: // some system errors
+			case WAIT_OBJECT_0 + 1:
+				cout << "Writing broken off" << endl;
+				exit = true;
+				break; // user has broken the ending off
+			case WAIT_TIMEOUT:
+				cout << "Timeout period " << TIMEOUT << "ms elapsed, nothing was received. Press a key to exit" << endl;
+				exit = true;
+				break; // timeout
+			default:
 				cout << "Reading failed, error " << GetLastError() << ". Press a key to exit" << endl;
 				exit = true;
-				break;
-			}
-		}
-		else {
-			NoData = false;
+				break; // some system errors
 		}
 		if (exit) {
 			break;
 		}
 		if (!NoData) {
-			string str = reply;
-
-			//parse group, subgroup
-			stringstream s(reply);
-			char group;
-			int subgroup;
-			s >> group >> subgroup;
-
-			//parse name
-			int open = str.find('<');
-			int close = str.find('>');
-			string name = str.substr(open + 1, close - open - 1);
-
-			//parse date
-			stringstream ss(str.substr(close + 2));
-			int d;
-			string m, y;
-			ss >> d >> m >> y;
-			vector<string> months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
-			int month = find(months.begin(), months.end(), m) - months.begin() + 1;
-			int year = stoi(y);
-
-			//make new date
-			Date* date = new Date(d, month, year);
-
-			//make new item
-			Item* itm = new Item(group, subgroup, name, *date);
-
+			inputs i = q.front();
+			switch (i) {
+			case c:
+				input = ready;
+				break;
+			case s:
+				input = stop;
+				break;
+			}
+			if (!WriteFile(hPipe, input, strlen(input) + 1, &nWritten, NULL))
+			{
+				cout << "Unable to write into file, error " << GetLastError() << endl;
+				return;
+			}
+			if (nWritten != strlen(input) + 1) {
+				cout << "Only " << nWritten << " bytes were written" << endl;
+				return;
+			}
+			q.pop();
 		}
 	}
-	CloseHandle(Overlapped.hEvent); // clean
-	delete reply;
 }
+
 
 
 void takeReply(HANDLE hPipe, HANDLE hExitEvent) {
@@ -263,7 +236,7 @@ int main()
 	HANDLE hExitEvent = CreateEventA(NULL, TRUE, FALSE, NULL);
 	HANDLE hHaveInput = CreateEventA(NULL, TRUE, FALSE, NULL);
 	thread listenInput{ takeInput, ref(q), hHaveInput, hExitEvent };
-	thread sendServer{ sendMsg, hPipe, ref(q), hHaveInput};
+	thread sendServer{ sendMsg, hPipe, ref(q), hHaveInput, hExitEvent};
 	thread takeOutput(takeReply, hPipe, hExitEvent);
 	listenInput.join();
 	sendServer.join();
